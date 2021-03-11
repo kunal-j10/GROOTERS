98306a8628fde296b094aea8e1285e76fbbb36ac
kunal-j10 <kunalsinghlegal@gmail.com>
2021-03-11 11:04:16 +0530

    adding app.js file

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Text,SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Navigator from './components/stack';
+
+
+
+const app=()=>{
+  return(
+    
+    <Navigator/>
+    
+  );
+}
+export default app;
